4fd41211b71bce157b02269f9322df15d4d08e1d
Michael Stelly <mjstelly@gmail.com>
2020-01-12 17:25:03 -0600

    creating unit test config files

--- src/screens/SearchScreen.test.js
+++ src/screens/SearchScreen.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import SearchScreen from './SearchScreen';
 
 describe('SearchScreen snapshot test', () => {
     it('renders correctly', () => {
+        const renderer = new ShallowRenderer();
+        renderer.render(<SearchScreen />);
 
-    })
+        const result = renderer.getRenderOutput();
+        expect(result).toMatchSnapshot();
+    });
 });
